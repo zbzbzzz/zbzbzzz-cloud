fb5cec04c24b5d1d05db10d186d54e11bf48f987
zb <42697182+1849633183@users.noreply.github.com>
2019-11-23 20:35:16 +0800

    Initial commit

---
+++ cloud-gateway/src/main/java/com/hope/CloudGatewayApplication.java
@@ -0,0 +1,60 @@
+package com.hope;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//@EnableConfigurationProperties(UriConfiguration.class)
+@SpringBootApplication
+//@RestController
+public class CloudGatewayApplication {
+
+	/** logger */
+	private static final Logger LOGGER = LoggerFactory.getLogger(CloudGatewayApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+		LOGGER.info("zbzbzzz-gateway 网关启动成功！温馨提示：代码千万行，注释第一行，命名不规范，同事泪两行");
+	}
+	/*@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+		String httpUri = uriConfiguration.getHttpbin();
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri(httpUri))
+				.route(p -> p
+						.host("*.hystrix.com")
+						.filters(f -> f
+								.hystrix(config -> config
+										.setName("mycmd")
+										.setFallbackUri("forward:/fallback")))
+						.uri(httpUri))
+				.build();
+	}
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
+	}
+*/
+}
+
+/*
+@ConfigurationProperties
+class UriConfiguration {
+
+	private String httpbin = "http://httpbin.org:80";
+
+	public String getHttpbin() {
+		return httpbin;
+	}
+
+	public void setHttpbin(String httpbin) {
+		this.httpbin = httpbin;
+	}
+}
+*/
